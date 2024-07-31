4f041e7feef8b32a8a458221771fbb5d77ed62f1
Xiao Jiang <bjjx1999@live.com>
2024-07-31 11:00:15 +0800

    Upgrade

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public abstract class Note {
+    public float time;
+    public float radius;
+}
+    
+[Serializable]
+public class RegularNote: Note {
+        
+}
+    
+[Serializable]
+public class SlideNote: Note {
+        
+}
+    
+[Serializable]
+public class CenterNote: Note {
+        
+}
+
+[Serializable]
+public class Path {
+    public string name;
+    public float delay;
+    public float duration;
+    public iTween.EaseType easeType;
+    public iTween.LoopType loopType;
+}
+    
+[CreateAssetMenu(fileName = "Level", menuName = "Alio/Level", order = 0)]
+public class Level : ScriptableObject {
+    [SerializeReference]
+    public List<Note> notes = new();
+    
+    public List<Path> paths = new();
+}
